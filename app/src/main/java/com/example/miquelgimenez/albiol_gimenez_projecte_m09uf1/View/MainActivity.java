5275c2e49f05f9c9a753759c94e68b56b09e9c89
miquelgimenez <miquel.nec31@gmail.com>
2017-03-30 21:00:39 +0200

    //IntentWorks

--- app/src/main/java/com/example/miquelgimenez/albiol_gimenez_projecte_m09uf1/View/MainActivity.java
+++ app/src/main/java/com/example/miquelgimenez/albiol_gimenez_projecte_m09uf1/View/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.miquelgimenez.albiol_gimenez_projecte_m09uf1.R;
@@ -12,23 +14,37 @@ import com.example.miquelgimenez.albiol_gimenez_projecte_m09uf1.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnFocusChange;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.btnStartChat) Button btnStartChat;
-
+    @BindView(R.id.etUsername) EditText etUsername;
+    @BindView(R.id.etChatRoom) EditText etChatRoo;
+    @BindView(R.id.swDefaultChatRoom) Switch swDefaultChatRoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        Toast.makeText(this, "666",Toast.LENGTH_LONG).show();
         ButterKnife.bind(this);
+
+        etChatRoo.setFocusable(false);
+
     }
+
+    @OnFocusChange(R.id.swDefaultChatRoom)
+    public void changeEtChatRoom(){
+        if(!(swDefaultChatRoom.isChecked())){
+            etChatRoo.setFocusable(false);
+        }else etChatRoo.setFocusable(true);
+    }
+
 
 
     @OnClick(R.id.btnStartChat)
     public void intentChatActivity(View view){
-        Toast.makeText(this, "jkowefnomsolmvñnvinj",Toast.LENGTH_LONG).show();
-        Intent chatAct_intent = new Intent(getApplicationContext(), ChatActivity.class);
-        startActivity(chatAct_intent);
+        if(etUsername.getText().toString().trim().length()>0){
+            Intent chatAct_intent = new Intent(this, ChatActivity.class);
+            startActivity(chatAct_intent);
+
+        }else Toast.makeText(this,"Has d'introduir un nom d'usuari per poder entrar!",Toast.LENGTH_LONG).show();
     }
 }
