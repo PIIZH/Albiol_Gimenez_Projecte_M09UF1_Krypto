03f265b4a1e23efa96caca9c91c3ba5fbcb04d68
gerard <gerardalbiol@gmail.com>
2017-04-15 16:36:56 +0200

    new class to encrypt o decrypt read 'TODO' message to generate keys

---
+++ app/src/main/java/com/example/miquelgimenez/albiol_gimenez_projecte_m09uf1/Controller/Encryptor.java
@@ -0,0 +1,62 @@
+package com.example.miquelgimenez.albiol_gimenez_projecte_m09uf1.Controller;
+
+import org.keyczar.Crypter;
+import org.keyczar.exceptions.KeyczarException;
+
+/**
+ * Created by gerard on 14/04/17.
+ */
+
+public class Encryptor {
+
+    private String textEnc; // text Encrypted
+    private String textDec; // text Decrypted
+    //TODO: estudiar com fer aixo!!!!
+    private static final String PATH = "path/de/les/nostres/keys";
+    Crypter cryptObj;
+
+    public Encryptor() {}
+
+    /**
+     * encrypt the message to send
+     *
+     * @param   {String}    text
+     * @return  {String or null}
+     */
+    public String encryptText(String text) {
+
+        try {
+            cryptObj = new Crypter(PATH);
+            this.textEnc = cryptObj.encrypt(text);
+            return this.textEnc;
+        }
+        catch(KeyczarException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+
+    }
+
+    /**
+     * decrypt the message received
+     *
+     * @param   {String}    text
+     * @return  {String or null}
+     */
+    public String decryptText(String text) {
+
+        try {
+            cryptObj = new Crypter(PATH);
+            this.textDec = cryptObj.decrypt(text);
+            return this.textDec;
+        }
+        catch(KeyczarException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+
+    }
+
+}
