7175cb5a939d9a9d5b74aee1d9077a6d9118e5c3
gerard <gerardalbiol@gmail.com>
2017-04-26 21:27:24 +0200

    sym class util

--- app/src/main/java/com/example/miquelgimenez/albiol_gimenez_projecte_m09uf1/Controller/Symetric/SymmetricUtil.java
+++ app/src/main/java/com/example/miquelgimenez/albiol_gimenez_projecte_m09uf1/Controller/Symetric/SymmetricUtil.java
@@ -59,28 +59,22 @@ public class SymmetricUtil {
 
         try {
             cripto.init(Cipher.ENCRYPT_MODE, myKey);
-//            textEncrypted = cripto.doFinal(message.getBytes());
-            textEncrypted = cripto.update(message.getBytes());
-            textEncrypted = cripto.doFinal();
-            return asHex(textEncrypted);
-//            return Base64.encodeToString(textEncrypted, NO_WRAP);
+            textEncrypted = cripto.doFinal(message.getBytes());
+            return Base64.encodeToString(textEncrypted, NO_WRAP);
         }
         catch(InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
 
-        return null;
+        return "encrypt ERROR";
 
     }
 
     public String decrypt(String msgEncrypt) {
         try {
-//            byte[] a = Base64.decode(msgEncrypt, NO_WRAP);
             cripto.init(Cipher.DECRYPT_MODE, myKey);
-//            cripto.update(a);
-//            textDecrypted = cripto.doFinal(Base64.decode(msgEncrypt, NO_WRAP));
-            textDecrypted = cripto.update(fromHexString(msgEncrypt));
-            textDecrypted = cripto.doFinal();
+            textDecrypted = cripto.doFinal(Base64.decode(msgEncrypt, NO_WRAP));
             return new String(textDecrypted);
         }
         catch(InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
@@ -88,32 +82,8 @@ public class SymmetricUtil {
             e.printStackTrace();
         }
 
-        return "no";
+        return "decrypt ERROR";
 
     }
 
-    public static byte[] fromHexString(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i + 1), 16));
-        }
-        return data;
-    }
-
-    public static String asHex(byte buf[]) {
-        StringBuffer strbuf = new StringBuffer(buf.length * 2);
-        int i;
-
-        for (i = 0; i < buf.length; i++) {
-            if (((int) buf[i] & 0xff) < 0x10) {
-                strbuf.append("0");
-            }
-
-            strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
-        }
-
-        return strbuf.toString();
-    }
 }
